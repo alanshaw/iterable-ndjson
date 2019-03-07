8ba8836c6303fc322275941048312b1cf41131ec
Alan Shaw <alan.shaw@protocol.ai>
2019-03-07 09:14:46 +0000

    feat: use TextDecoder in the browser
    Removes dependency on string_decoder and Buffer

--- browser.js
+++ browser.js
@@ -1,15 +1,15 @@
-const { StringDecoder } = require('string_decoder')
+/* eslint-env browser */
 
 module.exports = source => (async function * () {
   const matcher = /\r?\n/
-  const decoder = new StringDecoder('utf8')
+  const decoder = new TextDecoder('utf8')
   let buffer = ''
   for await (const chunk of source) {
-    buffer += decoder.write(chunk)
+    buffer += decoder.decode(chunk, { stream: true })
     const parts = buffer.split(matcher)
     buffer = parts.pop()
     for (let i = 0; i < parts.length; i++) yield JSON.parse(parts[i])
   }
-  buffer += decoder.end()
+  buffer += decoder.decode()
   if (buffer) yield JSON.parse(buffer)
 })()
